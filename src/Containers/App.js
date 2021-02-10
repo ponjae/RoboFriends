74cf1e9c0b004b9f6faefe22299c214e6e9be414
ponjae <>
2021-02-10 20:40:00 +0100

    Added function to add new robot

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -12,9 +12,13 @@ class App extends Component {
         super();
         this.state = {
             robots: [],
-            searchField: ''
+            searchField: '',
+            email: '',
+            name: '',
+            id: 11
         };
     }
+
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -24,6 +28,34 @@ class App extends Component {
 
     onSearchChange = (event) => {
         this.setState({ searchField: event.target.value });
+    }
+
+    handleNameChange = (e) => {
+        this.setState({name : e.target.value});
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({email: e.target.value});
+    }
+
+
+    onSubmit = (e) => {
+        this.setState({id: this.state.id+1});
+        let obj = {
+            "name" : this.state.name,
+            "id" : this.state.id,
+            "email" : this.state.email
+        }
+
+        this.setState({
+            robots : [...this.state.robots, obj]
+        });
+
+        e.preventDefault();
+        this.setState({
+            "name" : '',
+            "email" : ''
+        })
     }
 
     render() {
@@ -37,7 +69,11 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-
+                    <form onSubmit={this.onSubmit}>
+                            <input type='text' required id='nameField' value={this.state.name} onChange={this.handleNameChange} placeholder='Enter your name'/>
+                            <input type='text' required id='emailField' value={this.state.email} onChange={this.handleEmailChange} placeholder='Enter your email'/>
+                            <input type="submit" value="Submit"/>
+                    </form>
                     <Scroll>
                         <ErrorBoundary>
                             <CardList robots={filteredRobots} />
