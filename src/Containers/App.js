ffbe9aafddeb01fe9c09b12a577654cb60f9081c
ponjae <>
2021-03-09 15:32:12 +0100

    functionality for redux added but not implemented due to already existing extra functionality

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
 import ErrorBoundary from '../Components/ErrorBoundary';
 import './App.css';
+import { setSearchField, requestRobots } from '../actions';
 
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots : state.requestRobots.robots,
+        isPending : state.requestRobots.isPending,
+        error : state.requestRobots.error
+    }
+}
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
             robots: [],
-            searchField: '',
             email: '',
             name: '',
             id: 11
@@ -26,40 +41,37 @@ class App extends Component {
             .then(users => this.setState({ robots: users }));
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchField: event.target.value });
-    }
-
     handleNameChange = (e) => {
-        this.setState({name : e.target.value});
+        this.setState({ name: e.target.value });
     }
 
     handleEmailChange = (e) => {
-        this.setState({email: e.target.value});
+        this.setState({ email: e.target.value });
     }
 
 
     onSubmit = (e) => {
-        this.setState({id: this.state.id+1});
+        this.setState({ id: this.state.id + 1 });
         let obj = {
-            "name" : this.state.name,
-            "id" : this.state.id,
-            "email" : this.state.email
+            "name": this.state.name,
+            "id": this.state.id,
+            "email": this.state.email
         }
 
         this.setState({
-            robots : [...this.state.robots, obj]
+            robots: [...this.state.robots, obj]
         });
 
         e.preventDefault();
         this.setState({
-            "name" : '',
-            "email" : ''
+            "name": '',
+            "email": ''
         })
     }
 
     render() {
-        const { robots, searchField } = this.state;
+        const { robots } = this.state;
+        const {searchField, onSearchChange} = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase())
         });
@@ -68,11 +80,11 @@ class App extends Component {
             (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <form onSubmit={this.onSubmit}>
-                            <input type='text' className='pa3 ba b--green bg-lightest-blue' required id='nameField' value={this.state.name} onChange={this.handleNameChange} placeholder='Enter your name'/>
-                            <input type='text' className='pa3 ba b--green bg-lightest-blue' required id='emailField' value={this.state.email} onChange={this.handleEmailChange} placeholder='Enter your email'/>
-                            <input type="submit" className='pa3 ba b--green bg-lightest-blue' value="Submit"/>
+                        <input type='text' className='pa3 ba b--green bg-lightest-blue' required id='nameField' value={this.state.name} onChange={this.handleNameChange} placeholder='Enter your name' />
+                        <input type='text' className='pa3 ba b--green bg-lightest-blue' required id='emailField' value={this.state.email} onChange={this.handleEmailChange} placeholder='Enter your email' />
+                        <input type="submit" className='pa3 ba b--green bg-lightest-blue' value="Submit" />
                     </form>
                     <Scroll>
                         <ErrorBoundary>
@@ -84,4 +96,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
